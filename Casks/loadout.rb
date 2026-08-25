f5dbc2291504c079fe45eec20d8df0ecc979187f
Miguel Silva <85745515+migsilva89@users.noreply.github.com>
2026-08-25 08:55:38 +0100

    Loadout 0.3.1

--- Casks/loadout.rb
+++ Casks/loadout.rb
@@ -1,6 +1,6 @@
 cask "loadout" do
-  version "0.3.0"
-  sha256 "a0cf4a9d14d7e63de33ca2a252bc3cfbac95476677819fed6c2573bbb50fa4bc"
+  version "0.3.1"
+  sha256 "d29f0e9ce1e9af552ea2df2e0125b79ed49531e46f90a515190cb0c9880400b0"
 
   url "https://github.com/migsilva89/loadout/releases/download/v#{version}/Loadout-#{version}.dmg"
   name "Loadout"
